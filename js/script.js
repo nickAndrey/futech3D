e538adf24238d16c34d327a56cfba8d6c044f4f1
Black_Moon <nickandreyuser@gmail.com>
2017-06-17 12:27:40 +0200

    index-page commit; no mobile yet

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    var one = $('#owl-first');
+    var two = $('#owl-second');
+
+    one.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: [
+            '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+            '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+        ],
+    });
+
+    two.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: [
+            '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+            '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+        ],
+    });
+});
